e26936a0c0de17fbce58ceaa7206fac399fc6311
fealum <mail@feals.de>
2024-03-16 01:34:36 +0100

    Changed encyclopedia start page design

--- resources/views/components/encyclopedia/subpages.blade.php
+++ resources/views/components/encyclopedia/subpages.blade.php
@@ -1,6 +1,9 @@
 <ol>
 @foreach ($pages as $page)
     <li class="encyclopedia-{{ $page->id }}">
+        @if (!$page->parent)
+        <img src="{{ asset('img/encyclopedia/thumb-'.$page->id.'.jpg') }}" />
+        @endif
         <a href="{{ route('encyclopedia.view', ['page' => $page->id]) }}"
             @if (strlen($page->text) < 5)
             class="stub"
